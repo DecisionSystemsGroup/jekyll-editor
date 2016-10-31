520cd95855bb4a680e6c0b413baeed8cd13d61c1
Stavros Tsourlidakis <s.tsourlidakis@hotmail.com>
2016-10-31 14:44:20 +0200

    add npm start command

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('/', function(req, res){
 	res.render('main', {categories: categories});
 });
 
-var port = 3000;
+var port = 80;
 app.listen(port, function () {
 	console.log('App listening on port '+port+'!');
 });
